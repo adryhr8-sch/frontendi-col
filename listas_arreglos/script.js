291fe8b9a3366b83c1005537c54a27ec3193f17a
adryhr8-sch <adrian.hernandezramirez@cesunbc.edu.mx>
2026-01-30 09:44:25 -0800

    DOM vs BOM || Listas y arreglos

---
+++ listas_arreglos/script.js
@@ -0,0 +1,18 @@
+document.getElementById("mostrarLista").addEventListener("click", function() {
+    var secciónLista = document.getElementById("listaCiudades");
+    if (secciónLista.innerHTML) {
+        secciónLista.innerHTML = "";
+        return;
+    }
+    const ciudades = ["Nueva York", "Los Ángeles", "Chicago", "Houston", "Phoenix"];
+    const encabezado = document.createElement("h2");
+    encabezado.textContent = "Lista de Ciudades";
+    secciónLista.appendChild(encabezado);
+    const lista = document.createElement("ul");
+    ciudades.forEach(function(ciudad) {
+        const elementoLista = document.createElement("li");
+        elementoLista.textContent = ciudad;
+        lista.appendChild(elementoLista);
+    });
+    secciónLista.appendChild(lista);
+});
