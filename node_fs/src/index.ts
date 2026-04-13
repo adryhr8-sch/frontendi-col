2d20945f93ad35d12ad5a5cd0c0237402f4ae870
adryhr8-sch <adrian.hernandezramirez@cesunbc.edu.mx>
2026-04-13 15:23:43 -0700

    búsqueda de nombres

--- node_fs/src/index.ts
+++ node_fs/src/index.ts
@@ -1,8 +1,9 @@
 import { leer, escribir } from "./archivos";
 
 
+// Creación de archivo de nombres por entrada de usuario
+
 const archivo_datos: string = "datos.txt";
-const archivo_salida: string = "resultado.txt";
 
 var nombres: Array<string> = [];
 
@@ -22,7 +23,11 @@ const texto: string = nombres.join("\n");
 
 escribir(archivo_datos, texto);
 
-//
+console.log("El archivo de nombres se creó correctamente")
+
+// Archivo de estadísticas
+
+const archivo_stats: string = "resultado.txt";
 
 const datos_nombres: Array<string> = leer(archivo_datos).split("\n");
 
@@ -40,6 +45,21 @@ Nombre más largo: ${datos_nombres.reduce((a, b) => a.length >= b.length ? a : b
 Total de nombres: ${datos_nombres.length}
 `
 
-escribir(archivo_salida, texto_resultado);
+escribir(archivo_stats, texto_resultado);
 
 console.log("Proceso completado. Revisa el archivo resultado.txt para ver los resultados.");
+
+// Búsqueda de nombres
+
+console.log("Buscando nombres en el archivo...")
+
+const nombres_origen: Array<string> = leer(archivo_datos).split("\n");
+
+const query: string = prompt("Escribe un nombre para buscar:") || "";
+
+if (nombres_origen.includes(query)) {
+    const cantidad: number = nombres_origen.filter(nombre => nombre === query).length;
+    console.log(`El nombre "${query}" aparece ${cantidad} veces en el archivo.`);
+} else {
+    console.log(`El nombre "${query}" no se encuentra en el archivo.`);
+}
